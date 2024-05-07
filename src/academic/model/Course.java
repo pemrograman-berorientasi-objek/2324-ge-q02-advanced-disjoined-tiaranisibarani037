89f45c41eedffea8ace9b96164422822f3c6058f
tiaranisibarani037 <tiarani25sbrn@gmail.com>
2024-05-07 16:04:16 +0700

    Test_01

--- src/academic/model/Course.java
+++ src/academic/model/Course.java
@@ -1,11 +1,25 @@
 package academic.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S22037 Tiarani Sibarani
  */
-public class Course {
 
-    // class definition
+ public class Course extends Entity {
+    private String credit;
+    private String passingGrade;
+
+    public Course(String _course_id, String _course_name, double _credit, String _passingGrade) {
+        super(_course_id, _course_name);
+        this.credit = Integer.toString((int) _credit);
+        this.passingGrade = _passingGrade;
+    }
+
+    public String getCredit() {
+        return credit;
+    }
+
+    public String getPassingGrade() {
+        return passingGrade;
+    }
 
 }
--- src/academic/model/Student.java
+++ src/academic/model/Student.java
@@ -1,11 +1,23 @@
 package academic.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S22037 Tiarani Sibarani
  */
-public class Student {
+public class Student extends Entity {
+    private String year;
+    private String studyProgram;
 
-    // class definition
+    public Student(String _id, String _name, String _year, String _studyProgram) {
+        super(_id, _name);
+        this.year = _year;
+        this.studyProgram = _studyProgram;
+    }
 
+    public String getYear() {
+        return this.year;
+    }
+
+    public String getStudyProgram() {
+        return this.studyProgram;
+    }
 }
